85717c642c441a4a7d1b2735d64383f74e301667
cromero <cromero@digitalorigin.com>
2017-10-25 12:55:15 +0200

    updated php test

---
+++ test/DefaultTest.php
@@ -0,0 +1,16 @@
+<?php
+namespace Test;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class DefaultTest
+ * @package Test
+ */
+class DefaultTest extends TestCase
+{
+    public function testDefault()
+    {
+        $this->assertTrue(true);
+    }
+}
