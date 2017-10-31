36dcf7fc07f7525d283a1037d03ed8d3f99f9f68
cromero <cromero@digitalorigin.com>
2017-10-31 18:09:42 +0100

    Updated code

--- extension/app/code/community/DigitalOrigin/Pmt/Block/Form/Paylater.php
+++ extension/app/code/community/DigitalOrigin/Pmt/Block/Form/Paylater.php
@@ -1,7 +1,15 @@
 <?php
 
+/**
+ * Class DigitalOrigin_Pmt_Block_Form_Paylater
+ */
 class DigitalOrigin_Pmt_Block_Form_Paylater extends Mage_Payment_Block_Form
 {
+    /*
+     * Constructor for the form.
+     *
+     * Fetch data from configuration and send it to the view
+     */
     protected function _construct()
     {
         $quote = Mage::getModel('checkout/session')->getQuote();
@@ -9,8 +17,13 @@ class DigitalOrigin_Pmt_Block_Form_Paylater extends Mage_Payment_Block_Form
 
         $quoteData= $quote->getData();
         $amount=$quoteData['grand_total'];
+        $isProduction = $config['PAYLATER_PROD'];
+        $publicKey = $isProduction ? $config['PAYLATER_PUBLIC_KEY_PROD'] : $config['PAYLATER_PUBLIC_KEY_TEST'];
+        $simulatorType = $config['PAYLATER_CHECKOUT_HOOK_TYPE'];
 
-        $this->setData('total', $amount);
+        $this->setData('amount', $amount);
+        $this->setData('publicKey', $publicKey);
+        $this->setData('simulatorType', $simulatorType);
 
         $this->setTemplate('pmt/form/paylater.phtml')->setMethodTitle(Mage::helper('pmt')->__($config['title']));
 
