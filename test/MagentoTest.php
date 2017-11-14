a6737d1f303349d78eb5b866d8d6eb122922d3e2
cromero <cromero@digitalorigin.com>
2017-11-14 12:04:16 +0100

    updated from chrome to firefox

--- test/MagentoTest.php
+++ test/MagentoTest.php
@@ -55,7 +55,7 @@ abstract class MagentoTest extends TestCase
      */
     protected function setUp()
     {
-        $capabilities = array(WebDriverCapabilityType::BROWSER_NAME => 'chrome');
+        $capabilities = array(WebDriverCapabilityType::BROWSER_NAME => 'firefox');
         $this->webDriver = RemoteWebDriver::create('http://localhost:4444/wd/hub', $capabilities);
     }
 
