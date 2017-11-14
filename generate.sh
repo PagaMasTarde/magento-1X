127eeaf1cb626e059ac92126e3c7659cde1d77c2
cromero <cromero@digitalorigin.com>
2017-11-14 11:31:36 +0100

    Updated code

---
+++ generate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Prepare environment
+set -e
+docker-compose down
+docker-compose up -d
+sleep 30
+composer install
+
+# Build Package todo
+
+# Run test
+extension/lib/DigitalOrigin/bin/phpunit --group magento-basic
+extension/lib/DigitalOrigin/bin/phpunit --group magento-install
+extension/lib/DigitalOrigin/bin/phpunit --group magento-configure-backoffice
+extension/lib/DigitalOrigin/bin/phpunit --group magento-buy-unregistered
