2227e0f93c3bb75e37405f7a1a8b088e274164c5
cromero <cromero@digitalorigin.com>
2017-11-17 21:40:04 +0100

    updated with try

--- generate.sh
+++ generate.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# Prepare environment
+# Prepare environment and build package
 docker-compose down
 docker-compose up -d
 composer install
 npm install
-
-# Build Package todo
 grunt
-ls -lshc extension/var/connect/
 
 # Time to boot and install magento
 sleep 30
@@ -16,6 +13,7 @@ set -e
 
 docker exec -it magento1x_magento-test_1 sh -c "chmod -R 777 /var/www"
 docker exec -it magento1x_magento-test_1 sh -c "chmod -R 777 /pmt"
+docker exec -it magento1x_magento-test_1 sh -c "ls -lshc /pmt/var/connect/"
 # CLI install (maybe you want to try it also, otherwise it will be installed with backOffice)
 docker exec -it magento1x_magento-test_1 sh -c "chmod +x mage"
 docker exec -it magento1x_magento-test_1 sh -c "./mage channel-add http://connect20.magentocommerce.com/community"
