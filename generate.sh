49f0888738990e488f96b53d87ee5332abfe09c3
cromero <cromero@digitalorigin.com>
2017-11-17 19:30:14 +0100

    updated magento version and generate

--- generate.sh
+++ generate.sh
@@ -12,13 +12,13 @@ grunt
 ls -lshc extension/var/connect/
 
 # Time to boot and install magento
-sleep 130
+sleep 15
 
 # Install:
 
 # CLI install (maybe you want to try it also, otherwise it will be installed with backOffice)
 docker cp extension/var/connect/DigitalOrigin_Pmt.tgz magento1x_magento-test_1:/tmp/DigitalOrigin_Pmt.tgz
-docker exec -it magento1x_magento-test_1 sh -c "ls -ls /tmp"
+docker exec -it magento1x_magento-test_1 sh -c "chmod 777 /tmp/DigitalOrigin_Pmt.tgz && ls -lsh /tmp"
 docker exec -it magento1x_magento-test_1 sh -c "chmod +x mage"
 docker exec -it magento1x_magento-test_1 sh -c "./mage channel-add http://connect20.magentocommerce.com/community"
 docker exec -it magento1x_magento-test_1 sh -c "./mage install-file /tmp/DigitalOrigin_Pmt.tgz"
