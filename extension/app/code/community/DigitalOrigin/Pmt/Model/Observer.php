bc2a2b2157669046e15616abc22b8474dfe7bf26
cromero <cromero@digitalorigin.com>
2018-01-10 00:01:17 +0100

    added creation of categoty

--- extension/app/code/community/DigitalOrigin/Pmt/Model/Observer.php
+++ extension/app/code/community/DigitalOrigin/Pmt/Model/Observer.php
@@ -5,9 +5,13 @@
  */
 class DigitalOrigin_Pmt_Model_Observer
 {
+    const PROMOTIONS_CATEGORY = 'paylater-promotion-product';
 
     const AUTOLOADER_FILE = '/DigitalOrigin/autoload.php';
 
+    /**
+     * @return $this
+     */
     public function addAutoloader()
     {
         $baseDir = Mage::getBaseDir('lib');
@@ -15,6 +19,37 @@ class DigitalOrigin_Pmt_Model_Observer
         $autoload = $baseDir.$autoloadPath;
 
         require_once $autoload;
+
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    public function addCategory()
+    {
+        $promotionCategory = Mage::getResourceModel('catalog/category_collection')
+            ->addFieldToFilter('name', self::PROMOTIONS_CATEGORY)->getFirstItem();
+        if (!$promotionCategory instanceof Mage_Catalog_Model_Category || !$promotionCategory->getEntityId()) {
+            try {
+                /** @var Mage_Catalog_Model_Category $category */
+                $category = Mage::getModel('catalog/category');
+                $category->setName(self::PROMOTIONS_CATEGORY);
+                $category->setUrlKey('paylater');
+                $category->setIsActive(0);
+                $category->setDisplayMode(Mage_Catalog_Model_Category::DM_PRODUCT);
+                $category->setIsAnchor(0);
+                $category->setDescription(Mage::helper('pmt')->__('Los productos con esta categoría tienen
+                 financiacion gratis asumida por el comercio. Úsalo para promocionar tus productos.'));
+                $category->setStoreId(Mage::app()->getStore()->getId());
+                $parentId = Mage_Catalog_Model_Category::TREE_ROOT_ID;
+                $parentCategory = Mage::getModel('catalog/category')->load($parentId);
+                $category->setPath($parentCategory->getPath());
+                $category->save();
+            } catch (\Exception $e) {
+                echo "error al crear categoría";
+            }
+        }
 
         return $this;
     }
