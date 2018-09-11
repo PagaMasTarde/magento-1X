4da276dcd4809af3df1d04d478441c5e0895dea6
Raúl Marín <rmarin@digitalorigin.com>
2018-09-11 09:45:58 +0200

    new class DigitalOrigin_Pmt_LogController

---
+++ extension/app/code/community/DigitalOrigin/Pmt/controllers/LogController.php
@@ -0,0 +1,87 @@
+<?php
+
+/**
+ * Class DigitalOrigin_Pmt_ApiController
+ */
+class DigitalOrigin_Pmt_LogController extends Mage_Core_Controller_Front_Action
+{
+    /**
+     * @var string $message
+     */
+    protected $message;
+    /**
+     * @var bool $error
+     */
+    protected $error = false;
+
+    /**
+     * Controller index method:
+     */
+    public function indexAction()
+    {
+        if (!$this->authorize()) {
+            $result = json_encode(array('timestamp' => time(), 'result' => 'Access Forbidden'));
+            $headers = array(
+                'HTTP/1.1 403 Forbidden' => 403,
+                'Content-Type' => 'application/json',
+                'Content-Length' => strlen($result)
+            );
+            return $this->response($result, $headers);
+        }
+
+        $result = array('timestamp' => time(), result => array());
+        $logDir  = Mage::getBaseDir('var') . DIRECTORY_SEPARATOR . 'log';
+        if (is_dir($logDir)) {
+            $pmtLogFile = $logDir . DIRECTORY_SEPARATOR . 'pmt.log';
+            $prepmtLogFile = $logDir . DIRECTORY_SEPARATOR . 'pre-pmt.log';
+            if (file_exists($pmtLogFile)) {
+                $result['result']['pmt'] = file_get_contents($pmtLogFile);
+            }
+            if (file_exists($prepmtLogFile)) {
+                $result['result']['pre-pmt'] = file_get_contents($prepmtLogFile);
+            }
+
+        }
+
+        $result = json_encode($result);
+        $headers = array(
+            'HTTP/1.1 200 Ok' => 200,
+            'Content-Type' => 'application/json',
+            'Content-Length' => strlen($result)
+        );
+        return $this->response($result, $headers);
+    }
+
+    /**
+     * @return bool
+     * @throws Zend_Controller_Request_Exception
+     */
+    public function authorize()
+    {
+        $moduleConfig = Mage::getStoreConfig('payment/paylater');
+        $env = $moduleConfig['PAYLATER_PROD'] ? 'PROD' : 'TEST';
+        $privateKey = $moduleConfig['PAYLATER_PRIVATE_KEY_'.$env];
+        if ((Mage::app()->getRequest()->getParam('secret') == $privateKey ||
+            Mage::app()->getRequest()->getHeader('secret') == $privateKey)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Return and print the response of request
+     *
+     * @param string $result
+     * @param array  $headers
+     * @return Mage_Core_Controller_Response_Http
+     */
+    public function response($result = '', $headers = array()) {
+        $response = $this->getResponse();
+        $response->setBody($result);
+        foreach ($headers as $key => $value) {
+            $response->setHeader($key, $value);
+        }
+        return $response;
+    }
+}
