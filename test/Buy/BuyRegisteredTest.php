bb9be88e94e9e7e6245bf6c6fbd4a19639b300a4
pgarces <gspablo@gmail.com>
2018-12-21 13:02:56 +0100

    Test. Remove validation

--- test/Buy/BuyRegisteredTest.php
+++ test/Buy/BuyRegisteredTest.php
@@ -147,7 +147,6 @@ class BuyRegisteredTest extends AbstractBuy
         $this->assertNotEmpty($response->body->status_code, $response);
         $this->assertNotEmpty($response->body->timestamp, $response);
         $this->assertNotEmpty($response->body->merchant_order_id, $response);
-        $this->assertNotEmpty($response->body->pmt_order_id, $response);
         $this->assertContains(NoIdentificationException::ERROR_MESSAGE, $response->body->result, "PR=>".$response->body->result);
     }
 
