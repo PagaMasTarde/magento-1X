e1504409b0b001ceb59965fab4a40ffd92574cf1
romeritoCL <romero.latorre@gmail.com>
2019-05-23 15:43:48 +0200

    Updated gitignore, composer.json adn generate.sh

--- generate.sh
+++ generate.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
+composer install
 
 # Prepare environment and build package
 docker-compose down
-docker-compose up -d --build magento-test
-if [ $1 == 'true' ]
-then
-    docker-compose up -d --build selenium
-fi
+docker-compose up -d --build
+
 sleep 10
 
 # Copy Files to magento test
@@ -27,31 +25,26 @@ docker-compose exec magento-test ./n98-magerun.phar dev:symlinks 1
 
 set -e
 # Run test
-composer install
-
-if [ $1 == 'true' ]
-then
-    echo magento-basic
-    extension/lib/Pagantis/bin/phpunit --group magento-basic
-    echo magento-configure-backoffice-iframe
-    extension/lib/Pagantis/bin/phpunit --group magento-configure-backoffice
-    echo magento-product-page
-    extension/lib/Pagantis/bin/phpunit --group magento-product-page
-    echo magento-buy-unregistered
-    extension/lib/Pagantis/bin/phpunit --group magento-buy-unregistered
-    echo magento-cancel-buy-unregistered
-    extension/lib/Pagantis/bin/phpunit --group magento-cancel-buy-unregistered
-    echo magento-register
-    extension/lib/Pagantis/bin/phpunit --group magento-register
-    echo magento-fill-data
-    extension/lib/Pagantis/bin/phpunit --group magento-fill-data
-    echo magento-buy-registered
-    extension/lib/Pagantis/bin/phpunit --group magento-buy-registered
-    echo magento-cancel-buy-registered
-    extension/lib/Pagantis/bin/phpunit --group magento-cancel-buy-registered
-    echo magento-cancel-buy-controllers
-    extension/lib/Pagantis/bin/phpunit --group magento-cancel-buy-controllers
-else
-    echo magento-configure-backoffice-redirect
-    extension/lib/Pagantis/bin/phpunit --group magento-configure-backoffice
-fi
+echo magento-basic
+extension/lib/Pagantis/bin/phpunit --group magento-basic
+echo magento-configure-backoffice-iframe
+extension/lib/Pagantis/bin/phpunit --group magento-configure-backoffice
+echo magento-product-page
+extension/lib/Pagantis/bin/phpunit --group magento-product-page
+echo magento-buy-unregistered
+extension/lib/Pagantis/bin/phpunit --group magento-buy-unregistered
+echo magento-cancel-buy-unregistered
+extension/lib/Pagantis/bin/phpunit --group magento-cancel-buy-unregistered
+echo magento-register
+extension/lib/Pagantis/bin/phpunit --group magento-register
+echo magento-fill-data
+extension/lib/Pagantis/bin/phpunit --group magento-fill-data
+echo magento-buy-registered
+extension/lib/Pagantis/bin/phpunit --group magento-buy-registered
+echo magento-cancel-buy-registered
+extension/lib/Pagantis/bin/phpunit --group magento-cancel-buy-registered
+echo magento-cancel-buy-controllers
+extension/lib/Pagantis/bin/phpunit --group magento-cancel-buy-controllers
+# Generate Pakage
+echo magento-package
+extension/lib/Pagantis/bin/phpunit --group magento-package
