8fcf1b12550756a81c837de721d27cae427b4be6
pgarces <gspablo@gmail.com>
2019-08-21 11:10:24 +0200

    Remove load function

--- extension/app/code/community/Pagantis/Pagantis/Block/Checkout/Pagantis.php
+++ extension/app/code/community/Pagantis/Pagantis/Block/Checkout/Pagantis.php
@@ -21,7 +21,7 @@ class Pagantis_Pagantis_Block_Checkout_Pagantis extends Mage_Payment_Block_Form
         $promotedAmount =  0;
         $cart = Mage::getModel('checkout/cart')->getQuote();
         foreach ($cart->getAllVisibleItems() as $item) {
-            $magentoProduct = $item->getProduct();
+            $magentoProduct = $item->getProduct()->load();
             $pagantisPromoted = $magentoProduct->getData("pagantis_promoted") ? 1 : 0;
             $productPrice = $item->getRowTotalInclTax();
             if ($pagantisPromoted) {
