18cc6cbfc0cacd9e21c43d1a6562ab00c99514ef
pgarces <gspablo@gmail.com>
2019-09-02 16:39:22 +0200

    Database fields

--- extension/app/code/community/Pagantis/Pagantis/sql/pagantis_setup/install-8.2.3.php
+++ extension/app/code/community/Pagantis/Pagantis/sql/pagantis_setup/install-8.2.3.php
@@ -40,4 +40,25 @@ $installer->run("INSERT INTO `pagantis_config`
     ('PAGANTIS_SIMULATOR_THOUSANDS_SEPARATOR', '.'),
     ('PAGANTIS_SIMULATOR_DECIMAL_SEPARATOR', ',')");
 
+$setup = new Mage_Eav_Model_Entity_Setup('core_setup');
+$setup->addAttribute('catalog_product', $code, array(
+    'group'         => $group,
+    'input'         => 'boolean',
+    'type'          => 'int',
+    'label'         => $label,
+    'source'        => 'eav/entity_attribute_source_boolean',
+    'visible'       => true,
+    'visible_on_front' => false,
+    'required'      => false,
+    'user_defined'  => false,
+    'searchable'    => true,
+    'filterable'    => true,
+    'comparable'    => true,
+    'used_in_product_listing' => true,
+    'visible_in_advanced_search'  => true,
+    'is_html_allowed_on_front' => false,
+    'unique'        => false,
+    'global'        => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
+));
+
 $installer->endSetup();
