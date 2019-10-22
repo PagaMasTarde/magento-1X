c34fca4394b3b2adacd762871ca7fe2c36e30e49
rmairnleal <raul.marin.leal@gmail.com>
2019-10-22 10:21:52 +0200

    upgrade 8.2.5-8.2.6

---
+++ extension/app/code/community/Pagantis/Pagantis/sql/pagantis_setup/upgrade-8.2.5-8.2.6.php
@@ -0,0 +1,16 @@
+<?php
+
+/** @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+/* @var $installer Mage_Core_Model_Resource_Setup */
+
+$installer->startSetup();
+
+$installer->run('DROP TABLE IF EXISTS pagantis_cart_concurrency');
+$installer->run('CREATE TABLE `pagantis_cart_concurrency` (
+  `id` varchar(50) NOT NULL,
+  `timestamp` INT NOT NULL,
+  PRIMARY KEY (`id`)
+  )');
+
+$installer->endSetup();
