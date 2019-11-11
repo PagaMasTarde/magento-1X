aaff44325c702901298706dbf2103e06207ddb2d
rmairnleal <raul.marin.leal@gmail.com>
2019-11-11 17:33:32 +0100

    fix magento get query

--- extension/app/code/community/Pagantis/Pagantis/controllers/NotifyController.php
+++ extension/app/code/community/Pagantis/Pagantis/controllers/NotifyController.php
@@ -464,11 +464,11 @@ class Pagantis_Pagantis_NotifyController extends AbstractController
                     "SELECT TIMESTAMPDIFF(SECOND,NOW()-INTERVAL %s SECOND, FROM_UNIXTIME(timestamp)) as rest FROM %s WHERE %s",
                     self::CONCURRENCY_TIMEOUT,
                     self::CONCURRENCY_TABLENAME,
-                    "order_id=$orderId"
+                    "id=$orderId"
                 );
-                $conn = Mage::getSingleton('core/resource')->getConnection('core_write');
-                $resultSeconds = $conn->query($query);
-                $restSeconds = isset($resultSeconds) ? ($resultSeconds->rest) : 0;
+                $dbObject = Mage::getSingleton('core/resource')->getConnection('core_write');
+                $resultSeconds = $dbObject->fetchOne($query);
+                $restSeconds = isset($resultSeconds) ? ($resultSeconds) : 0;
                 $secondsToExpire = ($restSeconds>self::CONCURRENCY_TIMEOUT) ? self::CONCURRENCY_TIMEOUT : $restSeconds;
                 sleep($secondsToExpire+1);
                 $logMessage = sprintf(
@@ -477,7 +477,11 @@ class Pagantis_Pagantis_NotifyController extends AbstractController
                     self::CONCURRENCY_TIMEOUT,
                     $restSeconds
                 );
-                $this->insertLog(null, $logMessage);
+                $logEntry= new \Pagantis\ModuleUtils\Model\Log\LogEntry();
+                $logEntry->info($logMessage);
+                $model = Mage::getModel('pagantis/log');
+                $model->setData(array('log' => $logEntry->toJson()));
+                $model->save();
             }
         }
 
